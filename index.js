342adebabe24a79ba683182b381881fa057b6845
mangesh <mshanit@gmail.com>
2020-11-25 11:05:51 +0530

    added endpoints for cookbook and meal planner

--- index.js
+++ index.js
@@ -4,11 +4,23 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/',(req,res)=>{
     res.render("main/home.ejs");
+})
+app.get('/cookbook',(req,res)=>{
+    res.render("main/cookbook.ejs");
+})
+app.get('/planmeals',(req,res)=>{
+    res.render("main/planmeals.ejs");
 })
 
 app.listen(3000,()=>{
