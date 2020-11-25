0747bfb75dde302f07ab50c71ddadf7368ae908d
yoursshahnawaz <shahnawazgalaxy340@gmail.com>
2020-11-25 10:56:08 +0530

    repo restructuring

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/',(req,res)=>{
-    res.render("home.ejs");
+    res.render("main/home.ejs");
 })
 
 app.listen(3000,()=>{
